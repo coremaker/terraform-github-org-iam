5cc867c67b40f9c33d555452de59027fd8c08676
Cosmin Briscaru <cosmin@coremaker.io>
2022-09-23 16:57:19 +0100

    remove unused block

--- provider.tf
+++ provider.tf
@@ -6,12 +6,3 @@ terraform {
     }
   }
 }
-
-# provider "github" {
-#   owner = "coremaker"
-#   app_auth { 
-#     id              = var.github_app_id
-#     installation_id = var.github_app_installation_id
-#     pem_file        = var.github_app_pem_file
-#   }
-# }
