769726094045b6cdaac549b2cf298f9e1ccd27db
Cosmin Briscaru <cosmin@coremaker.io>
2022-09-23 15:47:40 +0100

    use fixed version

--- provider.tf
+++ provider.tf
@@ -1,13 +1,11 @@
-# terraform {
-#   required_version = "~> 1.0"
-
-#   required_providers {
-#     github = {
-#       source  = "integrations/github"
-#       version = "~> 4.0"
-#     }
-#   }
-# }
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "5.2.0"
+    }
+  }
+}
 
 # provider "github" {
 #   app_auth {} # When using `GITHUB_APP_XXX` environment variables
