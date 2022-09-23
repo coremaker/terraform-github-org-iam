6ff88ccf52efd1cd20d170cc31fa8150dcaa7f38
Cosmin Briscaru <cosmin@coremaker.io>
2022-09-23 15:54:06 +0100

    remove provider

--- provider.tf
+++ provider.tf
@@ -1,11 +1,11 @@
-terraform {
-  required_providers {
-    github = {
-      source  = "integrations/github"
-      version = "5.2.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     github = {
+#       source  = "integrations/github"
+#       version = "5.2.0"
+#     }
+#   }
+# }
 
 # provider "github" {
 #   app_auth {} # When using `GITHUB_APP_XXX` environment variables
