144a55e2f9b16d25b679913ff9bfb39d43bbfced
Tudor Mangri <tudor@coremaker.io>
2022-10-03 16:17:56 +0300

    Removed owner variable

--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
+variable "owner" {
+  description = "GitHub owner used to configure the provider"
+  type        = string
+  default     = "coremaker"
+}
+
 variable "user_list" { 
     default = {}
 }
