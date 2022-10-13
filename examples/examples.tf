01ebb0f505fac6430a3949a65b4377cdff774fa3
Tudor Mangri <tudor@coremaker.io>
2022-10-13 14:04:11 +0300

    Updated examples.tf file

--- examples/examples.tf
+++ examples/examples.tf
@@ -4,13 +4,15 @@ source = "../"
   # Users to be added in the organization
   organization_members = {
      "user1" = "admin"
-     "user2" = "member"
+     "user2" = "member"'
+     "user3" = "admin"
   }
 
   # Teams to be added in the org, along with user distribution per team
   team_members = {
     test-terraform = {
-        users = ["user1", "user2"]
+        members = ["user1", "user2"]
+        maintainers = ["user3"]
     }
   }
 }
